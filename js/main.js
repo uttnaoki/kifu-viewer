babe083e6612a98c424fe2d4f6c42a50f60add0c
Naoki Kinoshita <uttnaoki@gmail.com>
2018-11-22 21:20:52 +0900

    corsの問題を回避

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-const url = "https://shogiwars.heroz.jp/users/history/uttnaoki"
-$.ajax({
+const appendKifuURLs = (uid) => {
+  let url = `https://shogiwars.heroz.jp/users/history/${uid}`;
+  url = `http://cors-allow.azurewebsites.net/?url=${url}`;  // corsの問題を回避
+
+  $.ajax({
     url: url,
+    cache: false,
     success: (data) => {
-        $(data).find('.contents').each((index, val) => {
-            const sourceHref = 'https:' + $(val).find('.game_replay').find('a').attr('href');
-            const sourceHrefLen = sourceHref.length;
-            console.log(sourceHrefLen)
-            const kifuURL = 'http://swks.sakura.ne.jp/wars/kifu/' + sourceHref.substring(32, sourceHrefLen-10) + '.kif';
-            $('#url_list').append(`<div><a href=${kifuURL}>${kifuURL}</a></div>`)
-        })
+      $(data).find('.contents').each((index, val) => {
+        const sourceHref = 'https:' + $(val).find('.game_replay').find('a').attr('href');
+        const sourceHrefLen = sourceHref.length;
+        const kifuURL = 'http://swks.sakura.ne.jp/wars/kifu/' + sourceHref.substring(32, sourceHrefLen-10) + '.kif';
+        $('#url_list').append(`<div><a href=${kifuURL}>${kifuURL}</a></div>`)
+      })
     },
-});
+  });
+};
+
+appendKifuURLs('uttnaoki');
